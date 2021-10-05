8f2d21afda566523e3505824ca583e5880d0f9f2
Eric Torstenson <eric.s.torstenson@vumc.org>
2021-10-05 10:52:16 -0500

    Added support for writing bundles and to permit posts to find pre-existing record when an identifier is provided

--- ncpi_fhir_client/fhir_client.py
+++ ncpi_fhir_client/fhir_client.py
@@ -4,6 +4,7 @@ import pdb
 logger = logging.getLogger(__name__)
 from ncpi_fhir_utility.client import FhirApiClient
 import subprocess
+from time import sleep
 from pprint import pformat
 
 from ncpi_fhir_client.fhir_auth import get_auth
@@ -15,7 +16,7 @@ class InvalidCall(Exception):
         self.url = url
         self.response = response
 
-        super().__init__(f"HTTP {self.status_code} encountered")
+        super().__init__(f"HTTP {self.status_code} encountered ({url})")
 
 def ExceptOnFailure(success, url, response):
     if not success:
@@ -27,6 +28,7 @@ class FhirClient:
         """cfg is a dictionary containing all relevant details suitable for host and authentication"""
         self.host_desc = cfg.get('host_desc')
         self.auth = get_auth(cfg)
+        self.logger = logger
 
         if self.host_desc is None:
             self.host_desc = 'No Description'
@@ -42,9 +44,52 @@ class FhirClient:
         # Make the host desc suitable for filenames
         self.host_desc = self.host_desc.replace("/", "-").replace(" ", "_").lower()
 
+        # will remain None until a bundle file is initialized
+        self.bundle = None
+        
     def init_log(self):
         """make sure this uses the current logging, which probably changes based on user's input"""
         self.logger = logging.getLogger(__name__)
+
+
+    def init_bundle(self, bundle_filename, bundle_id):
+        self.bundle = open(bundle_filename, 'wt')
+        self.bundle.write("""{
+    "resourceType": "Bundle",
+    "id": \"""" + bundle_id + """\",
+    "type": "transaction",
+    "entry": [
+""")
+        self.write_comma = False
+
+    def write_to_bundle(self, resource):
+        response = deepcopy(resource)
+        if self.bundle:
+            # For now, let's just skip the ID so that it works in a more general sense
+            destination = f"{resource['resourceType']}" #/{resource['id']}"
+            if self.write_comma: 
+                self.bundle.write(",")
+            self.write_comma = True
+            #pdb.set_trace()
+            resource_data = dumps(resource)
+            full_url = f"""{self.target_service_url}/{resource['resourceType']}/{resource['id']}"""
+            self.bundle.write("""    {
+      "fullUrl": \"""" + full_url + """\",
+      "resource": """ + resource_data + """,
+      "request": {
+          "method": "POST",
+          "url": \"""" + destination + """\"
+      }
+    }""")
+        return response
+
+    def close_bundle(self):
+        if self.bundle:
+            self.bundle.write("""
+    ]
+}""")
+            self.bundle.close()
+
 
     def client(self):
         """Return cached client object, creating it if necessary"""
@@ -62,20 +107,32 @@ class FhirClient:
 
     def delete_by_query(self, resource, qry):
         reqargs = {}
-
+        pdb.set_trace()
         responses = []
         for response in self.get(f"{resource}?{qry}").entries:
             if "resource" in response:
                 responses.append(self.delete_by_record_id(resource, response['resource']['id']))
 
-        if len(response) == 1:
-            return response[0]
+        if len(responses) == 1:
+            return responses[0]
         return responses
 
+    def send_request(self, verb, api_path, body, reqargs=None, headers=None):
+        if headers is None:
+            headers = {}
+        if reqargs is None:
+            reqargs = {'headers': headers}
+
+        #headers = self.get_login_header(headers)
+        self.auth.update_request_args(reqargs)
+        headers = self.get_login_header(reqargs['headers'])
+        #pdb.set_trace()
+        return self.client().send_request(verb, api_path, json=body, headers=headers)
 
     def delete_by_record_id(self, resource, id):
         """Just a basic delete wrapper"""
         reqargs = {        }
+        #pdb.set_trace()
         self.auth.update_request_args(reqargs)
         endpoint = f"{self.target_service_url}/{resource}/{id}"
         success, result = self.client().send_request("delete", endpoint, **reqargs)
@@ -117,20 +174,30 @@ class FhirClient:
 
         return result        
 
-    def load(self, resource, data, validate_only=False):
+    def load(self, resource, data, validate_only=False, skip_insert_if_present=False):
         objs = data
 
         if not isinstance(objs, list):
             objs = [data]
 
         for obj in objs:
-            # First thing, let's go ahead and try deleting 
-            assert 'url' in data
-            url = data['url']
-            self.delete_by_query(resource, f"url={url}")
+            # Certainly don't want to delete anything if we are just validating something
+            # that may coincidentally overlap with the current resource's URL
+            if not validate_only:
+                # First thing, let's go ahead and try deleting 
+                assert 'url' in obj
+                url = obj['url']
+
+                responses = []
+                for response in self.get(f"{resource}?url={url}").entries:
+                    if "resource" in response:
+                        if skip_insert_if_present:
+                            return {'status_code': 201, 'response':response}
+                        responses.append(self.delete_by_record_id(resource, response['resource']['id']))
+
 
             kwargs = {
-                'json': data
+                'json': obj
             }
             
             self.auth.update_request_args(kwargs)            
@@ -145,10 +212,13 @@ class FhirClient:
 
             return result        
 
-    def post(self, resource, data, validate_only=False):
+    def post(self, resource, data, validate_only=False, identifier=None):
         """Basic POST wrapper
         
-           validate_only will append the $validate to the end of the final url"""
+           validate_only will append the $validate to the end of the final url
+           
+           providing an identifier will result in querying for an existing record
+           and replacing it"""
         objs = data
 
         if not isinstance(objs, list):
@@ -156,7 +226,7 @@ class FhirClient:
 
         for obj in objs:
             kwargs = {
-                'json': data
+                'json': obj
             }
             
             self.auth.update_request_args(kwargs)            
@@ -164,14 +234,29 @@ class FhirClient:
             if validate_only:
                 endpoint += "/$validate"
 
+            verb = "POST"
+            if identifier is not None:
+                #pdb.set_trace()
+                result = self.get(f"{resource}?identifier={identifier}")
+                # If it wasn't found, then we just plan to create
+                if result.success():
+                    if result.entry_count > 0:
+                        entry = result.entries[0]
+                        id = entry['resource']['id']
+                        print(f"Reusing the existing resource at {resource}:{id}")
+                        obj['id'] = id
+                        endpoint = f"{self.target_service_url}/{resource}/{id}"
+                        verb = "PUT"
+                        kwargs['json'] = obj
+
             success, result = self.client().send_request(
-                                "POST", 
+                                verb, 
                                 endpoint, 
                                 **kwargs)
 
             return result
 
-    def get(self, resource, recurse=True, rec_count=250, raw_result=False):
+    def get(self, resource, recurse=True, rec_count=-1, raw_result=False, reqargs=None):
         """Wrapper for basic http:get
 
         :param resource: FHIR Resource type 
@@ -195,10 +280,17 @@ class FhirClient:
             if "?" in resource:
                 count = f"&_count={rec_count}"
 
-        reqargs = {}
+        if reqargs is None:
+            reqargs = {}
+
         self.auth.update_request_args(reqargs)    
 
-        url = f"{self.target_service_url}/{resource}{count}"
+        if resource[0:4] == "http":
+            url = f"{resource}{count}"
+        else:
+            url = f"{self.target_service_url}/{resource}{count}"
+        #if 'headers' in reqargs:
+        #    print(f"Header for {url} : {reqargs['headers']}")
         success, result = self.client().send_request("GET", f"{url}", **reqargs)
        
         if not success:
@@ -221,4 +313,36 @@ class FhirClient:
             content.append(result)
         return content
 
+    def sleep_until(self, endpt_orig, target_count, sleep_time=5, timeout=360, message=""):
+        endpt = endpt_orig
+
+        n = 0
+        while True:
+            response = self.get(endpt, rec_count=-1)
+            entries = response.entries
+
+            if len(entries) > 0:
+                #pdb.set_trace()
+                # May (or may not) be a bundle...
+                if 'resourceType' in entries[0]:
+                    if entries[0]['resourceType'] == 'Bundle':
+                        if 'entry' in entries[0]:
+                            entries = entries[0]['entry']
+                        else:
+                            entries = []
+
+            #if len(entries) == target_count or n >= timeout:
+            #    return response
+            entry_count = len(entries)
+            if 'total' in response.response:
+                entry_count = response.response['total']
+            if entry_count == target_count or n >= timeout:
+                if n > 0:
+                    print(f"{n} seconds. ")
+                return response
+            if n == 0:
+                print(f"{message} - Waiting for {target_count}. Sleeping {sleep_time}", end='', flush=True)
+            n += sleep_time
+            sleep(sleep_time)
+            print(".", end='', flush=True)
 
--- ncpi_fhir_client/fhir_result.py
+++ ncpi_fhir_client/fhir_result.py
@@ -4,17 +4,24 @@ Provide some basic assistance with data responses from the fhir server
 
 
 """
-
+import pdb
 import subprocess
 from pprint import pformat
 
 class FhirResult:
     """Wrap the return value a bit to make interacting with it a bit more smoother"""
     def __init__(self, payload):
+        self.status_code = payload['status_code']
+        self.request_url = payload['request_url']
         self.response = payload['response']
 
+        #pdb.set_trace()
+        # Empty bundles don't have an entry
+        if 'total' in self.response and self.response['total'] == 0:
+            self.entries = []
+
         # Always return an array, even if it was a single entry
-        if 'entry' in self.response:
+        elif 'entry' in self.response:
             self.entries = self.response['entry']
         else:
             self.entries = [self.response]
@@ -29,6 +36,16 @@ class FhirResult:
             for ref in self.response['link']:
                 if ref['relation'] == 'next':
                     self.next = ref['url']       
+
+    def success(self, dump_error=False):
+        is_good = self.status_code > 199 and self.status_code < 300
+
+        if not is_good and dump_error:
+            print()
+            print(pformat(self.response))
+            print(f"{self.request_url} returned error code {self.status_code}")
+
+        return is_good
 
     def append(self, payload):
         """Extend our entry data by following pagination links"""
