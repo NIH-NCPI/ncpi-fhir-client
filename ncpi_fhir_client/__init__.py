a5e0a042c306c6d3838e8f8e46bdec4e6c0a3f49
Eric Torstenson <eric.s.torstenson@vumc.org>
2022-09-15 16:14:10 -0500

    Skip some oddball resource types that aren't likely to be manipulated that google doesn't handle gracefully

--- ncpi_fhir_client/__init__.py
+++ ncpi_fhir_client/__init__.py
@@ -4,6 +4,12 @@ __version__ = "0.1.1"
 
 _default_resources = None
 
+# Google seems to respond with some resources that it doesn't support queries for, 
+# so, since we are in a bit of a hurry, I'm just stashing those types here until
+# I can dig deeper into a possible way to identify resources that aren't queryable
+_invalid_resource_types = ['DomainResource', 'Resource']
+import pdb
+
 def default_resources(host, ignore_resources=['Bundle'], reset=False):
     global _default_resources 
 
@@ -19,7 +25,8 @@ def default_resources(host, ignore_resources=['Bundle'], reset=False):
             if 'resource' in restful_entry:
                 for resource in restful_entry['resource']:
                     # pdb.set_trace()
-                    _default_resources.append(resource['type'])
+                    if resource['type'] not in _invalid_resource_types:
+                        _default_resources.append(resource['type'])
 
     return [x for x in _default_resources if x not in ignore_resources]
     
