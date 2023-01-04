93367c4064cb3bacf38c52125eccf864c1d6d9d5
Eric Torstenson <eric.s.torstenson@vumc.org>
2023-01-04 10:38:47 -0600

    Moving requirements to setup to see if it helps with docker

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@ from setuptools import setup, find_packages
 
 from ncpi_fhir_client import __version__
 
-root_dir = os.path.dirname(os.path.abspath(__file__))
+"""root_dir = os.path.dirname(os.path.abspath(__file__))
 req_file = os.path.join(root_dir, "requirements.txt")
 with open(req_file) as f:
-    requirements = f.read().splitlines()
-
+    requirements = f.read().splitlines()"""
 setup(
     name="ncpi-fhir-client",
     version = __version__,
     description=f"NCPI FHIR Client {__version__}",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=requirements
+    install_requires=["requests", "jwt"]
 )
